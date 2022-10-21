2ca3a034b9c6847a715cd9a9e987366e575b3e50
김성길 <82089918+SungGil-5125@users.noreply.github.com>
2022-10-21 13:32:46 +0900

    :sparkles: :: project setting

---
+++ src/main/java/com/project/PokemonEncyclopedia/PokemonEncyclopediaApplication.java
@@ -0,0 +1,13 @@
+package com.project.PokemonEncyclopedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonEncyclopediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonEncyclopediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/PokemonEncyclopedia/PokemonEncyclopediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.PokemonEncyclopedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonEncyclopediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
