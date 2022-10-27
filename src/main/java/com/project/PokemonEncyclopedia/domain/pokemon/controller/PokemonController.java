d8203ffb1398e622c6c1ae4bb3c21fecb101ffc9
김성길 <82089918+SungGil-5125@users.noreply.github.com>
2022-10-27 09:19:24 +0900

    :sparkles: :: cors 오류 해결

--- src/main/java/com/project/PokemonEncyclopedia/domain/pokemon/controller/PokemonController.java
+++ src/main/java/com/project/PokemonEncyclopedia/domain/pokemon/controller/PokemonController.java
@@ -2,9 +2,12 @@ package com.project.PokemonEncyclopedia.domain.pokemon.controller;
 
 import com.project.PokemonEncyclopedia.domain.pokemon.Pokemon;
 import com.project.PokemonEncyclopedia.domain.pokemon.repository.PokemonRepository;
+import com.project.PokemonEncyclopedia.domain.pokemon.service.PokemonService;
 import com.project.PokemonEncyclopedia.global.api.PokemonFeignService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,10 +15,12 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin("http://localhost:3000")
 public class PokemonController {
 
     private final PokemonFeignService pokemonFeignService;
     private final PokemonRepository pokemonRepository;
+    private final PokemonService pokemonService;
 
     @GetMapping
     public String executeName() {
@@ -28,4 +33,8 @@ public class PokemonController {
         return pokemonRepository.findAll();
     }
 
+    @QueryMapping
+    public Pokemon findPokemonById(@Argument Long id) {
+        return pokemonService.findPokemonById(id);
+    }
 }
